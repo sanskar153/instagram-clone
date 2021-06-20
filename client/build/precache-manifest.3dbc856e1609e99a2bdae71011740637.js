6490f145f9c9014e8decf6084dde922622665040
sanskar153 <sanskaragrawalla@gmail.com>
2021-06-20 13:06:57 +0530

    commit

---
+++ client/build/precache-manifest.3dbc856e1609e99a2bdae71011740637.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d860ef2026418c2323cde40959f2ee52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fde1f310617d689a6bb",
+    "url": "/static/css/main.397da9f6.chunk.css"
+  },
+  {
+    "revision": "70c9184443c804ee3210",
+    "url": "/static/js/2.05df021f.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/static/js/2.05df021f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fde1f310617d689a6bb",
+    "url": "/static/js/main.0d28cc1a.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  }
+]);
